b3e00fc7fbab97fdc079c4aa7e3cdfe10a6ff7f9
Guillermo Valdes <guillermo@movimientolibre.com>
2025-01-30 15:54:21 -0600

    Otra correccion en email

--- buzones/buzon.py
+++ buzones/buzon.py
@@ -85,7 +85,7 @@ class Buzon:
         if self.ya_respondidos_con_acuses is False and len(self.mensajes) > 0:
             contador = 0
             for mensaje in self.mensajes:
-                if mensaje.ya_enviados_acuses is False and mensaje.email.lower() in remitentes:
+                if mensaje.ya_enviados_acuses is False and str(mensaje.email).lower() in remitentes:
                     mensaje.enviar_acuse(remitentes[mensaje.email])
                     contador += 1
             if contador > 0:
@@ -101,7 +101,7 @@ class Buzon:
         if self.ya_respondidos_con_rechazos is False and len(self.mensajes_descartados) > 0:
             contador = 0
             for mensaje in self.mensajes_descartados:
-                if mensaje.ya_enviados_rechazos is False and mensaje.email.lower() in remitentes:
+                if mensaje.ya_enviados_rechazos is False and str(mensaje.email).lower() in remitentes:
                     mensaje.enviar_rechazo(remitentes[mensaje.email])
                     contador += 1
             if contador > 0:
